5243fe87f22564603c9783540b34f1b29b810c5a
Umpoy <iumpoy@yahoo.com>
2019-05-01 13:06:10 -0700

    compressed first image

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
 const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
 
 gulp.task('message', () => {
     return console.log('Hello World');
 });
 
+// Copy HTML files
 gulp.task('copyHTML', () => {
     gulp.src('src/*.html')
         .pipe(gulp.dest('dist'));
+})
+
+// Optimize Images
+gulp.task('imageMin', () => {
+    gulp.src('src/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'))
 })
 
 gulp.task('default', () => {
