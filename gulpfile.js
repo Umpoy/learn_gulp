914fa80ce2b263b57559b71ca0bf8a9d852229b1
Umpoy <iumpoy@yahoo.com>
2019-05-01 09:15:59 -0700

    copied first html files

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,12 @@ const gulp = require('gulp');
 gulp.task('message', () => {
     return console.log('Hello World');
 });
+
+gulp.task('copyHTML', () => {
+    gulp.src('src/*.html')
+        .pipe(gulp.dest('dist'));
+})
+
+gulp.task('default', () => {
+    return console.log('Running gulp...');
+})
